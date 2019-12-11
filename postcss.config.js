e77300f9290782293a98b4807a0e25703b63368b
rbiiiii <foguenne.robin@gmail.com>
2019-12-11 13:39:40 +0100

    add postcss config

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: {
+        autoprefixer: {
+            browsers: [
+                "last 1 IE version",
+                "since 2012"
+            ]
+        }
+    },
+}

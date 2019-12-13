7b260e2d7d9fb2fcb1c30e7daa74e8793e2b8f73
rbiiiii <foguenne.robin@gmail.com>
2019-12-13 18:10:55 +0100

    change gulp conf + homepage desktop

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -15,3 +15,50 @@ const gallery = new Siema({
 
 document.querySelector('.prev').addEventListener('click', () => gallery.prev());
 document.querySelector('.next').addEventListener('click', () => gallery.next());
+
+/* var w = window.innerWidth,
+    perPage = 0,
+    resizeTimer,
+    gallery;
+
+function configureGallery(pageCount) {
+    
+    gallery = new Siema({
+        selector: '.gallery_slider',
+        duration: 200,
+        easing: 'ease-out',
+        perPage: 1,
+        startIndex: 0,
+        draggable: true,
+        multipleDrag: true,
+        threshold: 20,
+        loop: true,
+        rtl: false,
+        perPage: pageCount,
+        onInit: () => {},
+        onChange: () => {},
+    });    
+
+    document.querySelector('.prev').addEventListener('click', () => gallery.prev());
+    document.querySelector('.next').addEventListener('click', () => gallery.next());
+}
+
+function activateGallery() {
+    w = window.innerWidth;
+    if (w <= 980) {
+        configureGallery(1);
+    } else {
+        configureGallery(2);
+    }
+}
+
+activateGallery();
+
+window.addEventListener("resize", function(){
+	clearTimeout(resizeTimer);
+	resizeTimer = setTimeout(function() {
+        console.log('test 1');
+        activateGallery();
+        console.log('test 2');
+	}, 300);
+}); */
--- gulpfile.js
+++ gulpfile.js
@@ -12,20 +12,21 @@ function minify() {
 }
 
 const imgSrc = "static/assets/uploads/**";
+const dimensions = [ [400, 266], [620, 412], [768, 511], [1240, 824] ];
 
 function images(cb) {
-    [400, 620, 768, 1240].forEach(function (size) {
+  dimensions.forEach(function (size) {
       src(imgSrc)
-        .pipe(gulpNewer(`static/assets/uploadsOut/${size}`))
+        .pipe(gulpNewer(`static/assets/uploadsOut/${size[0]}`))
         .pipe(imagemin([    
             imagemin.jpegtran({progressive: true}),
             imageminMozjpeg({
                 quality: 80
             })
         ]))
-        .pipe(imageResize({ width: size }))
+        .pipe(imageResize({ width: size[0], height: size[1], upscale: true, crop: true }))
         .pipe(imagemin())
-        .pipe(dest(`static/assets/uploadsOut/${size}`))
+        .pipe(dest(`static/assets/uploadsOut/${size[0]}`))
     });
     cb();
   }

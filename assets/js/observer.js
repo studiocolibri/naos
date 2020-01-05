cac199843fd6c1ab8de09c7569630bcfba219ded
rbiiiii <foguenne.robin@gmail.com>
2020-01-05 13:32:50 +0100

    fix observer issue with mobile view in atelier

--- assets/js/observer.js
+++ assets/js/observer.js
@@ -6,13 +6,18 @@ if(!('IntersectionObserver' in window)) {
         observedItems[i].classList.add('is-visible');
     }
 } else {
+	let config = {
+		root: null,
+		rootMargin: '0px',
+		threshold: 0.5
+	};
 	let observer = new IntersectionObserver((entries, observer) => { 
 		entries.forEach(entry => {
-			if(entry.intersectionRatio > 0.5) {
+			if(entry.intersectionRatio > 0) {
 				entry.target.classList.add('is-visible');
 				observer.unobserve(entry.target);
 			}
 		});
-	}, {threshold: 0.85});
+	}, config);
 	document.querySelectorAll('.observed').forEach(obj => { observer.observe(obj) });
 }

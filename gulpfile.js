654452be8c6acb5d1139374677623764653b8d26
rbiiiii <foguenne.robin@gmail.com>
2020-01-19 14:20:46 +0100

    testing new gulp conf

--- gulpfile.js
+++ gulpfile.js
@@ -11,13 +11,13 @@ function minify() {
     .pipe(dest('public'));
 }
 
-let imgSrc = "static/assets/uploads/**";
+let imgSrc = "public/assets/uploads/**";
 let dimensions = [ 400, 620, 768, 1240 ];
 
 function images(cb) {
   dimensions.forEach(function (size) {
     src(imgSrc)
-      .pipe(gulpNewer(`static/assets/dest/${size}`))
+      .pipe(gulpNewer(`public/assets/dest/${size}`))
       .pipe(imagemin([    
           imagemin.jpegtran({progressive: true}),
           imageminMozjpeg({
@@ -25,7 +25,7 @@ function images(cb) {
           })
       ]))
       .pipe(imageResize({ width: size, upscale: false, crop: false }))
-      .pipe(dest(`static/assets/dest/${size}`))
+      .pipe(dest(`public/assets/dest/${size}`))
   });
   cb();
 }
